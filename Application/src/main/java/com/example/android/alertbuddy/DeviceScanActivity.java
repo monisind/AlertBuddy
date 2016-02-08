7f215155f050b997bcf646371e1246c1739b4c23
Monisha Gunalan <moni.sind@gmail.com>
2016-02-07 20:07:30 -0500

    removing progress bar

--- Application/src/main/java/com/example/android/alertbuddy/DeviceScanActivity.java
+++ Application/src/main/java/com/example/android/alertbuddy/DeviceScanActivity.java
@@ -72,16 +72,6 @@ public class DeviceScanActivity extends ListActivity {
         progress.setCancelable(false);
 
 
-//        progress = new ProgressDialog(this,R.style.progress);
-//        progress.setCancelable(false);
-////        progress.setMessage("Scanning");
-//        progress.setTitle("Scanning for AlertBuddy Device");
-
-
-
-
-
-
         // Use this check to determine whether BLE is supported on the device.  Then you can
         // selectively disable BLE-related features.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
